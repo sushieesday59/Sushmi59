60c2912f426f16b26414797c35db35c86ad6bba8
sushieesday59 <lsushmitha_cse220595@mgit.ac.in>
2024-11-25 20:47:49 -0800

    Add files via upload

---
+++ primitive.java
@@ -0,0 +1,25 @@
+class Primitivedatatypes
+{
+    public static void main(String args[])
+    {
+        boolean flag = true;
+        System.out.println(flag);
+        byte range=124;
+        System.out.println(range);
+        short temperature=-200;
+        System.out.println(temperature); 
+        int intrange = 4250000;
+        System.out.println(intrange);
+        long longrange = 42332200000L;
+        System.out.println(longrange);
+        double number = -42.3;
+        System.out.println(number);
+        float floatnumber = -42.3f;
+        System.out.println(floatnumber);
+        char letter = '\u0051';
+        System.out.println(letter);  // prints Q
+        char letter2 = 65;
+        System.out.println(letter2);  // prints A
+    
+    }
+}
